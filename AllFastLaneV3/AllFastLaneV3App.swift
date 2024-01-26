24eb6e038f8de9a73ee43af54aafdba2e3ccf30e
Ignacio Arias <1992ignacio@gmail.com>
2024-01-26 01:59:11 -0800

    Initial Commit

---
+++ AllFastLaneV3/AllFastLaneV3App.swift
@@ -0,0 +1,17 @@
+//
+//  AllFastLaneV3App.swift
+//  AllFastLaneV3
+//
+//  Created by Ignacio Arias on 2024-01-26.
+//
+
+import SwiftUI
+
+@main
+struct AllFastLaneV3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
